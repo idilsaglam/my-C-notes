1403af1196251d1fe1b0b64f995e9ee46261ac3a
idilsaglam <idilsaglam1@hotmail.com>
2022-02-20 15:35:43 +0100

    tp3 updated

--- src/TP3/tp3.c
+++ src/TP3/tp3.c
@@ -48,19 +48,24 @@ void nbr_occ_op (int nbr, int t[], int v, int *pnv){
 }
 
 void min_max_op(int nbr, int t[], int *pmin, int *pmax){
-    int min = t[0];
-    int max = t[0];
+    *pmin = *t;
+    printf("pmin = %d\n",*pmin);
+    *pmax = *t;
+    printf("pmax = %d\n",*pmax);
 
     for(int i =1; i<nbr; i++){
-        if(t[i]<min){
-            min = t[i];
+        if(t[i]<*pmin){
+            *pmin = *(t+i);
+            printf("pmin = %d\n",*pmin);
+
         }
-        if(t[i] > max){
-            max = t[i];
+        if(t[i] > *pmax){
+            *pmax = *(t+i);
+            printf("pmax = %d\n",*pmax);
         }
     }
-    *pmin = min;
-    *pmax = max;
+    printf("pmin = %d\n",*pmin);
+    printf("pmax = %d\n",*pmax);
 }
 
 
@@ -68,37 +73,49 @@ void print_tab (int t[], int start, int end){
     int* p = NULL;
     for(int i = start; i<end; i++){
         p = t+i;
-        printf("%p : %d", p, *p);
+        printf("%p : %d\n", p, *p);
     }
-    free(p);
 }
 
-void print_tab (int *pstart, int *pend){
+void print_tab2 (int *pstart, int *pend){
     for(int* i = pstart; i<=pend; i++){
         printf("%p : %d", i, *i);
     }
+    return;
 }
 int main(){
 
     int t[] = {2,3,4,5};
+
+    //TODO
+    swap(t,t+3);
+    print_tab(t,0,4);
+    int min;
+    int max;
+    min_max_op(4,t,&min, &max);
+    printf("max : %d min : %d\n",max,min);
+
+
     printf("test 1 %d\n", occ(4,t,4));
     printf("test 2 %d\n", occ(4,t,7));
 
-    printf("test 3 %p\n", occ_ptr(4,t,4));
-    printf("test 4 %p\n", occ_ptr(4,t,7));
 
-    int t2[] = {2,3,4,5,5,4,5,5};
-    printf("test 5 %d\n", nbr_occ(8,t2,5));
+    printf("test 3 %d\n", *occ_ptr(4,t,4));
+    printf("test 4 %d\n", *occ_ptr(4,t,7));
 
-    int x,y;
-    x=5; y=6;
-    printf("(avant) x: %d,y : %d\n",x,y);
-    swap(&x,&y);
-    printf("(apres) x : %d , y : %d\n",x,y);
+   int t2[] = {2,3,4,5,5,4,5,5};
+   printf("test 5 %d\n", nbr_occ(8,t2,5));
 
-    int tab[] = {1,2,3};
-    swap(tab[2],tab[0]);
-    printf("tab[2]= %d, tab[0] = %d\n",tab[2],tab[0]);
+   int x,y;
+   x=5; y=6;
+   printf("(avant) x: %d,y : %d\n",x,y);
+
+   swap(&x,&y);
+   printf("(apres) x : %d , y : %d\n",x,y);
+
+  int tab[] = {1,2,3};
+  swap(tab+2,tab);
+  printf("tab[2]= %d, tab[0] = %d\n",tab[2],tab[0]);
 
     return  0;
 }
