188e5c8efff042254658ff9adb5447b08754a387
idilsaglam <idilsaglam1@hotmail.com>
2022-02-26 19:49:14 +0100

    TP4 updated

--- src/TP4/tp4.c
+++ src/TP4/tp4.c
@@ -2,6 +2,9 @@
 // Created by Idil Saglam on 2/22/22.
 //
 #include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
 struct array {
     int *ptr;
     unsigned int capacite;
@@ -22,6 +25,45 @@ array* array_init(unsigned int cap){
     }
     return a;
 }
+
+void array_destroy(array *t){
+    free(t->ptr);
+    free(t);
+}
+
+int array_get(array* t,unsigned int index){
+    assert(index < t->taille);
+    return *(t+index);
+}
+
+void array_set(array* t, unsigned int index, int valeur){
+    assert(taille > index+1);
+    t[index] = valeur;
+    // *(t+index) = valeur;
+}
+void array_append(array* t, int val){
+    assert(taille<t->capacite);
+    *(t+taille) = val:
+}
+void array_print(array* t){
+    for(int i = 0; i<t->taille; i++){
+        printf(" %d",t->ptr[i]);
+        //*((t->ptr)+i)
+        t++;
+    }
+}
+
+int array_search(array *t, int val){
+    for(int i = 0; i<t->taille; i++){
+        if(t->ptr[i] == val){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+
 int main(){
     return 1;
 }
